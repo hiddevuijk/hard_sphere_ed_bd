c4d48d4035515d8f4a8e490e718f8256a2153378
hiddevuijk <hdvuijk@gmail.com>
2023-01-04 19:33:40 +0100

    Remove z velocity for testing

--- systemEDBD.h
+++ systemEDBD.h
@@ -299,6 +299,8 @@ void SystemEDBD<Potential>::UpdateVelocities(double dt)
     velocities_[i].z +=
        sqrt_2_dt * random_normal_distribution_();
 
+    // REMOVE
+    velocities_[i].z = 0;
   }
   
 }
@@ -449,6 +451,10 @@ void SystemEDBD<Potential>::MakeCollision(unsigned int p1, unsigned int p2)
   velocities_[p1] -= n_perp * a;
   velocities_[p2] += n_perp * a;
 
+  // REMOVE
+  velocities_[p1].z = 0;
+  velocities_[p2].z = 0;
+  
 }
 
 
